4e1fe00ca7c1d99b42109d68cfcacdc5c6ed5091
Lasha Butikashvili <lasha.butikashvili@gmail.com>
2025-07-25 20:31:24 +0400

    all

---
+++ main.js
@@ -0,0 +1,43 @@
+
+
+
+let score;
+
+score=0;
+
+console.log("Initial score:", score);
+
+score +=5;
+
+console.log("first half:", score);
+
+
+
+
+let finalScore;
+finalScore = score;
+
+console.log(`Final score: ${finalScore}`);
+
+
+const animals= ["dog", "cat", "bird"];
+
+console.log("animals array at the starting point", animals);
+
+animals.push("fish");
+
+console.log("Updated animals:", animals);
+
+const firstanimal = "hamster";
+
+console.log("The first animal should be a", firstanimal);
+
+animals.unshift(firstanimal);
+
+console.log("Animals after adding first animal:", animals);
+
+
+
+
+
+
