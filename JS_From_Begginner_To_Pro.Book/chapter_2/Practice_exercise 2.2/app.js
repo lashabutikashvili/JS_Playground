82a4119aec2b3ec0c8297bebbecde722cb011a2a
Lasha Butikashvili <lasha.butikashvili@gmail.com>
2025-07-27 14:04:42 +0400

    Practice_Exercise_2.2

---
+++ JS_From_Begginner_To_Pro.Book/chapter_2/Practice_exercise 2.2/app.js
@@ -0,0 +1,10 @@
+// alert("Practice Exercise 2.2 is now Connected");
+
+let myName = "Maaike";
+let myAge = 29;
+let codingJs = true;
+
+
+console.log("Hello, my name is ", myName, ", I am ", myAge, "years old and I can code Javascript: ", codingJs);
+
+console.log("EASY WORK")
