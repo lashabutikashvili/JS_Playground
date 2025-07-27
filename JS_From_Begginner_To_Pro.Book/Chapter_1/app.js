ef086918a12ef47ef8f83ea254a3de9238a8f051
Lasha Butikashvili <lasha.butikashvili@gmail.com>
2025-07-27 11:45:57 +0400

    26jul

---
+++ JS_From_Begginner_To_Pro.Book/Chapter_1/app.js
@@ -0,0 +1,9 @@
+
+
+// prompt("type your name here:");
+
+console.log(Math.floor(Math.random() * 100 + 23));
+
+document.getElementById("button").onclick = function() {
+    alert("Button clicked!");
+}
---
+++ JS_From_Begginner_To_Pro.Book/Chapter_1/chapter_project/app.js
@@ -0,0 +1,9 @@
+
+
+// if this file is connected to rhe index.html 
+// alert me 
+//here is multiple line comment 
+alert("JS Document connected");
+
+// my name in to the console
+// console.log("my name is Lasha");
