83e5e786ce6658ee4e6c4ee7aa6ce4808dccaeb9
Lasha Butikashvili <lasha.butikashvili@gmail.com>
2025-07-29 21:22:10 +0400

    End_of_chapter_2

---
+++ JS_From_Begginner_To_Pro.Book/chapter_2/Chapter_Project_BMICalculator/app.js
@@ -0,0 +1,11 @@
+// alert("connected to BMI Calc")
+
+alert("set your value in inches in the next prompt");
+
+let inchValue = prompt ("enter inch value you want to convert to CM:")
+console.log(`the value you entered in CM will be: ${inchValue * 2.54}`)
+
+alert("set your value in pounds in the next prompt");
+
+let kgValue = prompt("enter kg value you want to convert to pound:")
+console.log(`The value you entered in kg will be ${kgValue * 2.2046} in pounds`)
---
+++ JS_From_Begginner_To_Pro.Book/chapter_2/Chapter_Project_MilesToKm/app.js
@@ -0,0 +1,10 @@
+// alert("connected to chapter project")
+
+let miles = prompt ("Enter Miles you want to convert to KM: ");
+
+// let miles = 23;
+let kms = miles * 1.60934;
+
+alert('See the answer in the browser logs!');
+
+console.log(`The distance of ${miles} miles is equal to ${kms} kilometers.`);
