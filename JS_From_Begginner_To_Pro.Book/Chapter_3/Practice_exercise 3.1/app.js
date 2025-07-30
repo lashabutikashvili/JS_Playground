1fec85c4a7bb9ff6968f70ac0f53b94b51dc57d5
Lasha Butikashvili <lasha.butikashvili@gmail.com>
2025-07-30 17:52:03 +0400

    07/30

---
+++ JS_From_Begginner_To_Pro.Book/Chapter_3/Practice_exercise 3.1/app.js
@@ -0,0 +1,9 @@
+// alert("I am connected to the practice exercise 3.1");
+
+
+shoping_list = ["Milk", "Bread", "Apples"];
+console.log("My shopping list length is:", shoping_list.length)
+
+shoping_list[1] = "Banana";
+
+console.log(shoping_list);
